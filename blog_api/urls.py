545a4da98efe3b1685e9818ce454aaa60fe4a0fc
brightonchebor <cheborbrigton805@gmail.com>
2024-06-20 19:57:35 +0300

    updated

---
+++ blog_api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import PostlList, PostDetail
+
+app_name = 'blog_api'
+
+urlpatterns = [
+    path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
+    path('', PostlList.as_view(), name='listcreate'),
+]
+
